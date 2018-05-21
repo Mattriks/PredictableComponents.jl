57da6541d64aecc17ac9ecabaa7ded64f5381db2
Mattriks <Mattriks@users.noreply.github.com>
2018-05-22 04:59:09 +1000

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,7 @@ deploydocs(
     repo = "github.com/Mattriks/PredictableComponents.jl.git",
     target = "build",
     julia = "0.6",
+    osname = "linux",
     deps = nothing,
     make = nothing,
 )
